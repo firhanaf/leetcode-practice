44145a5e693520e0c735e52619cb4b7142984e9c
Firhan Ahmad F <firhanahmadf@gmail.com>
2024-05-16 13:00:21 +0700

    Time: 1 ms (78.01%), Space: 2.1 MB (71.73%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.go
@@ -0,0 +1,13 @@
+func numberOfSteps(num int) int {
+    step := 0
+    for num > 0 {
+        if num % 2 == 0 {
+            num = num / 2
+            step++
+        } else {
+            num = num - 1
+            step++
+        }
+    }
+    return step
+}
